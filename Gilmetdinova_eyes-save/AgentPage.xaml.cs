874412030f1983d09896a52167414b1c13c75883
m1runas <gilmetdinovaruslana@gmail.com>
2023-10-25 14:25:45 +0500

    9 и 10 лаба

---
+++ Gilmetdinova_eyes-save/AgentPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gilmetdinova_eyes_save
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+
+            var currentAgent = Gilmetdinova_eyesEntities.GetContext().Agent.ToList();
+            AgentListView.ItemsSource = currentAgent;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
