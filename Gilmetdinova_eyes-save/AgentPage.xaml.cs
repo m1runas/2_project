52c45effb37b8aab336c7fc111df0498001f02a4
JeyBarrette <144144826+m1runas@users.noreply.github.com>
2023-11-10 10:57:59 +0500

    12 лаба постраничный вывод

--- Gilmetdinova_eyes-save/AgentPage.xaml.cs
+++ Gilmetdinova_eyes-save/AgentPage.xaml.cs
@@ -21,6 +21,13 @@ namespace Gilmetdinova_eyes_save
     /// </summary>
     public partial class AgentPage : Page
     {
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+
+        List<Agent> CurrentPageList = new List<Agent>();
+        List<Agent> TableList;
+
         public AgentPage()
         {
             InitializeComponent();
@@ -67,14 +74,14 @@ namespace Gilmetdinova_eyes_save
         {
             var currentAgent = Gilmetdinova_eyesEntities.GetContext().Agent.ToList();
 
-            if(ComboType.SelectedIndex == 0)
+            if (ComboType.SelectedIndex == 0)
             {
                 currentAgent = currentAgent.Where(p => (p.AgentTypeID >= 1 && p.AgentTypeID <= 6)).ToList();
             }
 
             if (ComboType.SelectedIndex == 1)
             {
-                currentAgent = currentAgent.Where(p => (p.AgentTypeID==1)).ToList();
+                currentAgent = currentAgent.Where(p => (p.AgentTypeID == 1)).ToList();
             }
 
             if (ComboType.SelectedIndex == 2)
@@ -89,21 +96,21 @@ namespace Gilmetdinova_eyes_save
 
             if (ComboType.SelectedIndex == 4)
             {
-                currentAgent = currentAgent.Where(p => (p.AgentTypeID ==4)).ToList();
+                currentAgent = currentAgent.Where(p => (p.AgentTypeID == 4)).ToList();
             }
 
             if (ComboType.SelectedIndex == 5)
             {
-                currentAgent = currentAgent.Where(p => (p.AgentTypeID ==5)).ToList();
+                currentAgent = currentAgent.Where(p => (p.AgentTypeID == 5)).ToList();
             }
 
             if (ComboType.SelectedIndex == 6)
             {
-                currentAgent = currentAgent.Where(p => (p.AgentTypeID ==6)).ToList();
+                currentAgent = currentAgent.Where(p => (p.AgentTypeID == 6)).ToList();
             }
 
-   
-          
+
+
 
             if (ComboType2.SelectedIndex == 0)
             {
@@ -132,23 +139,131 @@ namespace Gilmetdinova_eyes_save
 
             if (ComboType2.SelectedIndex == 5)
             {
-              //  currentAgent = currentAgent.OrderByDescending(p => p.Скидка).ToList();
+                //  currentAgent = currentAgent.OrderByDescending(p => p.Скидка).ToList();
             }
 
             if (ComboType2.SelectedIndex == 6)
             {
-             //   currentAgent = currentAgent.OrderBy(p => p.Скидка).ToList();
+                //   currentAgent = currentAgent.OrderBy(p => p.Скидка).ToList();
             }
-           
+
 
             currentAgent = currentAgent.Where(p =>
             p.Title.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Phone.Replace("+7", "7").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "").Contains(TBoxSearch.Text.Replace("+7", "8").Replace("(", "").Replace(")", "").Replace(" ", "").Replace("-", "")) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
             AgentListView.ItemsSource = currentAgent;
+
+            TableList = currentAgent;
+
+            ChangePage(0, 0);
         }
 
         private void ComboType2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             UpdateAgent();
+        }
+
+
+        private void ChangePage(int diraction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % 10 > 0)
+            {
+                CountPage = CountRecords / 10 + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / 10;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                    for (int i = CurrentPage * 10; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (diraction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * 10 + 10 < CountRecords ? CurrentPage * 10 + 10 : CountRecords;
+                            for (int i = CurrentPage * 10; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+            
+
+                AgentListView.ItemsSource = CurrentPageList;
+
+                AgentListView.Items.Refresh();
+            }
+        }
+    
+        
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+            
+        }
+
+        private void RightDitButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+          
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString())-1);
+            
         }
     }
 }
